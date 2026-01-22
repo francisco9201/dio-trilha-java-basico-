7fb94fbc8f5a3122dbf4bac3806f574cf280ca4b
Francisco Maciel <francisco-maciel92@hotmail.com>
2026-01-22 17:41:01 -0300

    https://github.com/francisco9201/dio-trilha-java-basico-/commit/b17036c9f11bf6568fd817839fde8d25c152b06e

--- conta_banco/ContaTerminal.java
+++ conta_banco/ContaTerminal.java
@@ -1,6 +1,29 @@
+import java.util.Scanner;
+
+
 public class ContaTerminal {
     
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Olá, bem-vindo ao nosso Banco!");
+
+        System.out.println("Por favor, digite o número da Conta:");
+        int numero = sc.nextInt();
+
+
+        System.out.println("Por favor, digite o número da Agência:");
+        String agencia = sc.next();
+
+        System.out.println("Por favor, digite o nome do Cliente:");
+        String nomeCliente = sc.next();
+
+        System.out.println("Por favor, digite o saldo da Conta:");
+        double saldo = sc.nextDouble();
+
+        System.out.println("Olá " + nomeCliente + 
+                           ", obrigado por criar uma conta em nosso banco. Sua agência é " 
+                           + agencia + ", conta " + numero + 
+                           " e seu saldo é R$ " + saldo + "Já está disponìvel para saque.");
+
     }
 }
